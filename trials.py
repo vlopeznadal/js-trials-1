7b20ddaa11ac4df75444f0890ee96083150f6be6
Virginia Lopez Nadal <vlopeznadal@icloud.com>
2021-10-18 12:21:01 -0500

    Completed 9 of 11 functions

--- trials.py
+++ trials.py
@@ -1,45 +1,71 @@
 """Python functions for JavaScript Trials 1."""
 
-
 def output_all_items(items):
-    pass  # TODO: replace this line with your code
-
+    for item in items:
+        print(f"{item}")
 
 def get_all_evens(nums):
-    pass  # TODO: replace this line with your code
+    even_nums = []
 
+    for num in nums:
+        if num % 2 == 0:
+            even_nums.append(num)
+    return even_nums
 
 def get_odd_indices(items):
-    pass  # TODO: replace this line with your code
-
+    result = []
+    for i in range(len(items)):
+        if i % 2 != 0:
+            result.append(items[i])
+    return result
 
 def print_as_numbered_list(items):
-    pass  # TODO: replace this line with your code
+    i = 1
 
+    for item in items:
+        print(f"{i}. {item}")
+        i += 1
 
 def get_range(start, stop):
-    pass  # TODO: replace this line with your code
-
+    nums = []
+    for i in range(start,stop):
+        nums.append(i)
+    print(nums)
 
 def censor_vowels(word):
-    pass  # TODO: replace this line with your code
+    chars = []
 
+    for letter in word:
+        if letter in "aeiou":
+            chars.append("*")
+        else:
+            chars.append(letter)
+
+    return " ".join(chars)
 
 def snake_to_camel(string):
-    pass  # TODO: replace this line with your code
-
+    camel_case = []
+    for word in string.split("_"):
+        camel_case.append(f"{word[0].upper()}{word[1:]}")
+    return "".join(camel_case)
 
 def longest_word_length(words):
-    pass  # TODO: replace this line with your code
+    longest = len(words[0])
 
+    for word in words:
+        if longest < len(word):
+            longest = len(word)
+    return longest
 
 def truncate(string):
-    pass  # TODO: replace this line with your code
-
+    result = []
+    for char in string:
+        if len(result) == 0 or char != result[len(result)-1]:
+            result.append(char)
+    return "".join(result)
 
 def has_balanced_parens(string):
     pass  # TODO: replace this line with your code
-
 
 def compress(string):
     pass  # TODO: replace this line with your code
